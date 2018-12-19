ca689edc450531f59009cdc61b2b423b347160d7
Michael Nutt <mnutt346@gmail.com>
2018-12-19 15:38:11 -0600

    Update get request route.

--- server/index.js
+++ server/index.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const path = require('path');
 const bodyParser = require('body-parser')
 const db = require('../database/index.js')
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('pet/info', (req, res) => {
+app.get('api/info', (req, res) => {
     //extract pet_id from request
     //call db.getPetById(pet_id);
     //send response with pet info to client
