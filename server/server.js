8d0a6944384a33eae46e2f5ab2898e4c8885d4a5
Michael Nutt <mnutt346@gmail.com>
2018-12-21 19:46:11 -0600

    Add cors package to allow CORS requests.

--- server/server.js
+++ server/server.js
@@ -1,18 +1,22 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const model = require('../database/models/Pet_Info.js')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const model = require('../database/models/Pet_Info.js');
+const cors = require('cors');
+const app = express();
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/api/info', (req, res) => {
   // extract pet_id from request
-  let pet_id = req.body.pet_id
+  let pet_id = req.headers.pet_id
+  console.log('req in service GET: ', req.headers.pet_id)
 
   // call model.getPetById(pet_id);
   model.getPetById(pet_id, petInfo => {
     // send response with pet info to client
+    // console.log('PET INFO IN SERVICE GET: ', petInfo)
     res.status(200).send(petInfo)
   })
 })
