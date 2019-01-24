fd9192499d0391ff8c9cff2ad587d0ef62685d90
gizmo <gizmo@Gremlin.local>
2019-01-24 11:26:29 -0600

    modified ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,34 @@
+// module.exports = {
+//     apps: [{
+//         name: 'Pet-Info',
+//         script: './server/server.js'
+//     }],
+//     deploy: {
+//         production: {
+//             user: 'ubuntu',
+//             host: 'ec2-52-90-48-243.compute-1.amazonaws.com',
+//             key: '~/.ssh/Petsy_Info_Service_Keys.pem',
+//             ref: 'origin/master',
+//             repo: 'https://github.com/mnutt346/Pet-Info.git',
+//             path: '/home/ubuntu/Pet-Info',
+//             'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+//         }
+//     }
+// }
+
 module.exports = {
     apps: [{
-        name: 'Pet-Info',
+        name: 'pet-info',
         script: './server/server.js'
     }],
     deploy: {
         production: {
             user: 'ubuntu',
-            host: 'ec2-52-90-48-243.compute-1.amazonaws.com',
-            key: '~/.ssh/Petsy_Info_Service_Keys.pem',
+            host: 'ec2-18-223-252-165.us-east-2.compute.amazonaws.com',
+            key: '~/.ssh/petsy.pem',
             ref: 'origin/master',
-            repo: 'https://github.com/mnutt346/Pet-Info.git',
-            path: '/home/ubuntu/Pet-Info',
+            repo: 'git@github.com:Petsy2/Pet-Info.git',
+            path: '/home/ubuntu/pet-info',
             'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
         }
     }
